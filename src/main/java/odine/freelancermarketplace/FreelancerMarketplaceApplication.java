2599fd9c2f651d42ec508fb3208b416093367eaa
niyazi-eren <neren3@gatech.edu>
2025-01-09 11:39:40 +0300

    init

---
+++ src/main/java/odine/freelancermarketplace/FreelancerMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package odine.freelancermarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancerMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancerMarketplaceApplication.class, args);
+	}
+
+}
